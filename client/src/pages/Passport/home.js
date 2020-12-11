5a7cae287a4dc8448378800dd449962431c9c5f9
Giovanna Arizpe <veliaarizpe@gmail.com>
2020-12-10 22:27:40 -0600

    updating package

--- client/src/pages/Passport/home.js
+++ client/src/pages/Passport/home.js
@@ -35,7 +35,7 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 function Home() {
-    useStyles();
+    const classes = useStyles();
     return(
         <ThemeProvider theme={theme}>        
             <CssBaseline />

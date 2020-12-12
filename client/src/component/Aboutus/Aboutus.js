ed895ec29c07f3c15778700fc28fc9f13f4b1898
Giovanna Arizpe <veliaarizpe@gmail.com>
2020-12-12 13:49:37 -0600

    science

--- client/src/component/Aboutus/Aboutus.js
+++ client/src/component/Aboutus/Aboutus.js
@@ -48,6 +48,9 @@ function Aboutus(){
                                About Us
                             </Typography>
                             <Typography gutterBottom variant="h6" component="h4" className={classes.typography}>
+                            <a href = 'https://www.cnbc.com/2020/03/20/coronavirus-wake-up-millennials-prioritize-your-mental-health-right-now-says-psychotherapist.html'>"According to a 2019 report from the Blue Cross Blue Shield Association, major depression diagnoses are rising at a faster rate for millennials — a 47% increase since 2013 — compared to any other age group. And a 2018 survey from the American Psychiatry Association found that they are by and large the most anxious generation."</a><br/><br/>
+
+
                             In these trying times, not everyone has access to needed resources. Train of Thought understands this limitation and has created this space with these users in mind.<br/><br/>
 
                             Train of Thought implores users to sit down and elaborate on their thoughts and feelings on any subject. With our journal entry section, users have free reign to vent out any frustrations or highlight special parts of their day. <br/><br/>

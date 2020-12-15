b962caff1866add1d4589af3ff6499aa1b46389d
Angel Alexander <angale22@gmail.com>
2020-12-14 22:05:32 -0600

    fixing fonts and textarea containers, readding props

--- client/src/component/journal/SadJournalEntry.js
+++ client/src/component/journal/SadJournalEntry.js
@@ -1,13 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import {
-    Button, 
-    Card, 
-    CssBaseline, 
-    Grid, 
-    Paper, 
-    TextField, 
-    ThemeProvider 
+    Button,
+    Card,
+    CssBaseline,
+    Grid,
+    Paper,
+    TextField,
+    ThemeProvider
 } from '@material-ui/core';
 import Typography from '@material-ui/core/Typography';
 import theme from '../../theme';
@@ -15,20 +15,20 @@ import API from '../../utils/API'
 
 const useStyles = makeStyles((theme) => ({
     root: {
-      flexGrow: 1,
-      height: "100vh",
+        flexGrow: 1,
+        height: "100vh"
     },
     typography: {
         fontFamily: [
-          'Shrikhand',
-          'cursive',
+            'Shrikhand',
+            'cursive',
         ],
-        color: 'darkblue', 
+        color: 'darkblue',
     },
     paper: {
-      padding: theme.spacing(2),
-      textAlign: 'center',
-      color: theme.palette.secondary,
+        padding: theme.spacing(2),
+        textAlign: 'center',
+        color: theme.palette.secondary,
     },
     card: {
         maxWidth: 600,
@@ -37,9 +37,23 @@ const useStyles = makeStyles((theme) => ({
         height: 0,
         paddingTop: '56.25%', // 16:9
     },
+    p: {
+        fontSize: '100%',
+        fontWeight: 100,
+        fontFamily: [
+            'Roboto Condensed',
+            'sans-serif',
+        ],
+        color: 'darkblue',
+    },
+    textfield: {
+        marginLeft: theme.spacing(1),
+        marginRight: theme.spacing(1),
+        width: '80%'
+    },
 }));
 
-function SadJournalEntry( { desc, what, unpack, note } ) {
+function SadJournalEntry({ desc, what, unpack, note }) {
 
     const [entries, setEntries] = useState({})
 
@@ -49,121 +63,138 @@ function SadJournalEntry( { desc, what, unpack, note } ) {
         unpack: '',
         note: ''
     })
-// Load all entries and store them with setEntries
-  useEffect(() => {
-    loadEntries()
-  }, [])
-  // Loads all entries and sets them to entries
+    // Load all entries and store them with setEntries
+    useEffect(() => {
+        loadEntries()
+    }, [])
+    // Loads all entries and sets them to entries
     function loadEntries() {
-    API.getEntriesEmo()
-        .then(res => 
-        setEntries(res.data)
-        )
-        .catch(err => console.log(err));
+        API.getEntriesEmo()
+            .then(res =>
+                setEntries(res.data)
+            )
+            .catch(err => console.log(err));
     };
     function handleInputChange(event) {
         const { name, value } = event.target;
-        setFormObject({...formObject, [name]: value})
+        setFormObject({ ...formObject, [name]: value })
     };
     function handleFormSubmit(event) {
         event.preventDefault();
-          API.saveEntryEmo({
+        API.saveEntryEmo({
             mood: "Sad",
             what: formObject.what,
             unpack: formObject.unpack,
             note: formObject.note
-          })
+        })
             .then(() => setFormObject({
-        what: '',
-        unpack: '',
-        note: ''
+                what: '',
+                unpack: '',
+                note: ''
             }))
             .then(() => loadEntries())
             .catch(err => console.log(err));
-      };
+    };
     const classes = useStyles();
 
     return (
         <ThemeProvider theme={theme}>
-        <CssBaseline />
+            <CssBaseline />
             <div className={classes.root}>
-                    <Grid item xs={12}>
-                        <Card>
-                            <Grid item xs={12}>
-                                <Paper className={classes.paper}>
-                                    <Typography variant="h3" component="h2" className={classes.typography}>
-                                        So You're Upset...
+                <Grid item xs={12}>
+                    <Card>
+                        <Grid item xs={12}>
+                            <Paper className={classes.paper}>
+                                <Typography variant="h3" component="h2" className={classes.typography}>
+                                    So You're Upset...
                                     </Typography>
-                                    <Typography gutterBottom variant="h6" component="h2" className={classes.typography}>
-                                    {desc}
-                                    </Typography>
-                                </Paper>
-                            </Grid>
-                            <Grid item xs={12}>
-                                <Paper className={classes.paper}>
-                                    <Typography gutterBottom variant="h5" component="h2" className={classes.typography}>
-                                    {what}
-                                    </Typography>
-                                    <TextField
-                                        onChange={handleInputChange}
-                                        id="outlined-secondary"
-                                        label="Today I am sad because..."
-                                        variant="outlined"
-                                        color="primary"
-                                        name="what"
-                                        value={formObject.what}
-                                    />
-                                </Paper>
-                            </Grid>
-                            <Grid item xs={12}>
-                                <Paper className={classes.paper} controlId="exampleForm.ControlTextarea1">
-                                    <Typography gutterBottom variant="h5" component="h2" className={classes.typography}>
-                                    {unpack}
-                                    </Typography>
-                                    <TextField
-                                        onChange={handleInputChange}
-                                        id="outlined-secondary"
-                                        label="Actually, I also felt..."
-                                        variant="outlined"
-                                        color="primary"
-                                        name="unpack"
-                                        value={formObject.unpack}
-                                    />
-                                </Paper>
-                            </Grid>
-                            <Grid item xs={12}>
-                                <Paper className={classes.paper}>
-                                    <Typography gutterBottom variant="h5" component="h2" className={classes.typography}>
-                                    {note}
-                                    </Typography>
-                                    <TextField
-                                        onChange={handleInputChange}
-                                        id="outlined-secondary"
-                                        label="One more thing..."
-                                        variant="outlined"
-                                        color="primary"
-                                        name="note"
-                                        value={formObject.note}
-                                    />
-                                </Paper>
-                            </Grid>
-                            <Grid container>
+                                <Typography gutterBottom variant="h6" component="h2">
+                                    <p classname={classes.p}>
+                                        {desc}
+                                    </p>
+                                </Typography>
+                            </Paper>
+                        </Grid>
+                        <Grid item xs={12}>
+                            <Paper className={classes.paper}>
+                                <Typography gutterBottom variant="h5" component="h2">
+                                    <p classname={classes.p}>
+                                        {what}
+                                    </p>
+                                </Typography>
+                                <TextField
+                                    multiline
+                                    rowsMax="4"
+                                    className={classes.textfield}
+                                    onChange={handleInputChange}
+                                    id="outlined-secondary"
+                                    label="Today I feel sad because..."
+                                    variant="outlined"
+                                    color="primary"
+                                    name="what"
+                                    value={formObject.what}
+                                />
+                            </Paper>
+                        </Grid>
+                        <Grid item xs={12}>
+                            <Paper className={classes.paper} controlId="exampleForm.ControlTextarea1">
+                                <Typography gutterBottom variant="h5" component="h2">
+                                    <p classname={classes.p}>
+                                        {unpack}
+                                    </p>
+                                </Typography>
+                                <TextField
+                                    multiline
+                                    rowsMax="4"
+                                    className={classes.textfield}
+                                    onChange={handleInputChange}
+                                    id="outlined-secondary"
+                                    label="Actually, I also felt..."
+                                    variant="outlined"
+                                    color="primary"
+                                    name="unpack"
+                                    value={formObject.unpack}
+                                />
+                            </Paper>
+                        </Grid>
+                        <Grid item xs={12}>
+                            <Paper className={classes.paper}>
+                                <Typography gutterBottom variant="h5" component="h2">
+                                    <p classname={classes.p}>
+                                        {note}
+                                    </p>
+                                </Typography>
+                                <TextField
+                                    multiline
+                                    rowsMax="4"
+                                    className={classes.textfield}
+                                    onChange={handleInputChange}
+                                    id="outlined-secondary"
+                                    label="One more thing..."
+                                    variant="outlined"
+                                    color="primary"
+                                    name="note"
+                                    value={formObject.note}
+                                />
+                            </Paper>
+                        </Grid>
+                        <Grid container>
                             <Grid item xs={12}>
                                 <Paper className={classes.paper} controlId="exampleForm.ControlTextarea1">
                                     <Button
                                         onClick={handleFormSubmit}
                                         variant="primary"
                                         type="submit">
-                                            Submit
+                                        Submit
                                     </Button>
                                 </Paper>
                             </Grid>
-                            <br/>
-                            <br/>
+                            <br />
+                            <br />
                         </Grid>
                     </Card>
                 </Grid>
-            </div>  
+            </div>
         </ThemeProvider>
     )
 }
--- client/src/pages/HappyJournalPage/HappyJournalPage.js
+++ client/src/pages/HappyJournalPage/HappyJournalPage.js
@@ -6,24 +6,30 @@ import MediaCard from "../../component/media/MediaCard";
 import PlayerApp from '../../component/PlayerComponent/Player';
 import Header from "../../component/HeaderComponent/Header";
 import { Grid } from '@material-ui/core';
- 
+
 class HappyJournalPage extends Component {
   render() {
     return (
       <div>
         <Router>
-          <Header/>
-            <Grid container spacing={3}>
-              <Grid item xs={3}>
-                <MediaCard>
-                  <PlayerApp/>
-                </MediaCard>
-              </Grid>
-              <Grid item xs={8}>
-                <HappyJournalEntry/>
-              </Grid>
+          <Header />
+          <Grid container spacing={3}>
+            <Grid item xs={3}>
+              <MediaCard>
+                <PlayerApp />
+              </MediaCard>
             </Grid>
-          <BottomAppBar/>
+            <Grid item xs={8}>
+              <HappyJournalEntry
+                desc="Happiness is our brain’s way of telling us that 
+                something is good for us and encourages us to seek it out.  
+                We feel joy when the neurotransmitters dopamine and serotonin are released into our bodies."
+                what="What are you happy about today? How has that impacted your day?"
+                unpack="What are some of the things you did to make yourself happy today? Was your overall emotion different from yesterday? How?"
+                note="Feel free to type anything else here that you would like to note about today." />
+            </Grid>
+          </Grid>
+          <BottomAppBar />
         </Router>
       </div>
     );
